777f4c6c45e12985304645e7491170462b2ae558
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-11-02 11:56:58 +0900

    ソリューションを更新
    (1) プロジェクトの名前変更に対応

--- VideoConverter/My Project/Application.Designer.vb
+++ VideoConverter/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ViewVb.MainView
+            Me.MainForm = Global.VideoConverter.MainView
         End Sub
     End Class
 End Namespace
