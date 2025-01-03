8f7dbb2cd3cf6ea0804c6c8453b9ae43821fc0b0
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-01-03 10:40:13 +0900

    作業途中

---
+++ View/WinFormsControl/AudioConfig.cs
@@ -0,0 +1,13 @@
+﻿
+namespace WinFormsControl
+{
+
+public partial class AudioConfig : UserControl
+{
+    public AudioConfig()
+    {
+        InitializeComponent();
+    }
+}
+
+}   //  End of namespace  WinFormsControl
