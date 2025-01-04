8328a415b040dab830cd8d9fa80d79e570ecadca
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-01-04 23:39:11 +0900

    プロパティプロシージャを実装

--- View/WinFormsControl/EncodeConfig.cs
+++ View/WinFormsControl/EncodeConfig.cs
@@ -10,6 +10,27 @@ public partial class EncodeConfig : UserControl
     {
         InitializeComponent();
     }
+
+    //----------------------------------------------------------------
+    //    AudioConfig プロパティ
+    //
+    [Browsable(true)
+    ]
+    public AudioConfig AudioConfig
+    {
+        get { return this.wfcAudio; }
+    }
+
+    //----------------------------------------------------------------
+    //    VideoConfig プロパティ
+    //
+    [Browsable(true)
+    ]
+    public VideoConfig VideoConfig
+    {
+        get { return this.wfcVideo; }
+    }
+
 }
 
 }   //  End of namespace  WinFormsControl
