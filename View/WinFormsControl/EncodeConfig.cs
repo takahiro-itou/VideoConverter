28e9a8e5ea6de5bf8bb8b0524a1d85566ab772ec
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-01-04 23:24:22 +0900

    作業途中

---
+++ View/WinFormsControl/EncodeConfig.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Windows.Forms;
+
+namespace WinFormsControl
+{
+
+public partial class EncodeConfig : UserControl
+{
+    public EncodeConfig()
+    {
+        InitializeComponent();
+    }
+}
+
+}   //  End of namespace  WinFormsControl
