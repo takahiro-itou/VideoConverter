7018a94949953c1dafd458191522044219ffeab4
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-01-06 20:55:17 +0900

    プロパティプロシージャを実装

--- View/WinFormsControl/PathEdit.cs
+++ View/WinFormsControl/PathEdit.cs
@@ -12,6 +12,31 @@ public partial class PathEdit : UserControl
         InitializeComponent();
     }
 
+    /**
+    **    参照ボタンを押した時に表示するダイアログの種類
+    **/
+    public enum EDialogType
+    {
+        Folder = 0,     /**<  フォルダ          **/
+        OpenFile = 1,   /**<  ファイルを開く    **/
+        SaveFile = 2,   /**<  名前を付けて保存  **/
+    };
+
+    private EDialogType m_dialogType    = DialogType.Folder;
+
+    //----------------------------------------------------------------
+    //    DialogType  プロパティ
+    //
+    [Browsable(true)
+      , Description("ダイアログの種類を切り替える")
+      , Category("動作")
+    ]
+    public EDialogType DialogType
+    {
+        get { return  this.m_dialogType; }
+        set { this.m_dialogType = value; }
+    }
+
 }
 
 }   //  End of namespace  WinFormsControl
