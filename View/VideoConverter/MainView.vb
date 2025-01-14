50f3748d1e2b69d3cda679b67f097d42eb067988
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-01-14 20:13:08 +0900

    動作テスト用のダミーのイベントハンドラを追加

--- View/VideoConverter/MainView.vb
+++ View/VideoConverter/MainView.vb
@@ -8,4 +8,20 @@ Private Sub mnuFileExit_Click(sender As Object, e As EventArgs) Handles _
     Application.Exit()
 End Sub
 
+Private Sub mnuRunCount_Click(sender As Object, e As EventArgs) Handles _
+            mnuRunCount.Click
+''--------------------------------------------------------------------
+''    テスト用のダミー
+''--------------------------------------------------------------------
+Dim value As WinFormsControl.OperationPanel.EOrientation
+
+    value = Me.wfcOperation.Orientation
+    if (value <> WinFormsControl.OperationPanel.EOrientation.Vertical) Then
+        value = WinFormsControl.OperationPanel.EOrientation.Vertical
+    Else
+        value = WinFormsControl.OperationPanel.EOrientation.Horizontal
+    End If
+    Me.wfcOperation.Orientation = value
+End Sub
+
 End Class
