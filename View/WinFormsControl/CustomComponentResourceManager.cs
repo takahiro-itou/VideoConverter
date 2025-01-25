f506606a394b2b37872a84c79b0c1d5d0a487617
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-01-25 22:19:38 +0900

    ファイルを新規作成

---
+++ View/WinFormsControl/CustomComponentResourceManager.cs
@@ -0,0 +1,31 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  Video Converter.  ---                      **
+**                                                                      **
+**          Copyright (C), 2024-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of CustomComponentResourceManager class.
+**
+**      @file       CustomComponentResourceManager.cs
+**/
+
+using System.ComponentModel;
+
+namespace WinFormsControl
+{
+
+//========================================================================
+//
+//    CustomComponentResourceManager  class.
+//
+
+}
