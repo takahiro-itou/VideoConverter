1bf4f60ddf57b69b4fadb515a958533697883c45
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-01-27 19:49:20 +0900

    リファクタリング
    (1) コメントを追加

--- View/WinFormsControl/VideoConfig.cs
+++ View/WinFormsControl/VideoConfig.cs
@@ -1,4 +1,23 @@
-﻿
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  Video Converter.  ---                      **
+**                                                                      **
+**          Copyright (C), 2024-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of VideoConfig class.
+**
+**      @file       VideoConfig.cs
+**/
+
 using System.Windows.Forms;
 
 namespace WinFormsControl
